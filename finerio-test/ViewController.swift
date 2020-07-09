daee4483bf169e8a58d67f28a357836c82b03a4c
Macintosh HD <macintoshhd@MacBook-Pro-de-Macintosh.local>
2020-07-08 22:01:28 -0500

    Initial Commit

---
+++ finerio-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finerio-test
+//
+//  Created by Macintosh HD on 08/07/20.
+//  Copyright © 2020 vicentesiis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
